6cf65283fd2cc3840efb04963cad3c646b3681aa
pedrolucass99 <pedrorochi73@gmail.com>
2019-01-03 17:58:43 -0300

    ME SALVA

---
+++ app/Http/Controllers/DoctorController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DoctorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $doctor=\App\Doctor::all();
+        return view('doctor/index',compact('doctor'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+       return view('doctor/create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $doctor= new \App\Doctor;
+        $doctor->name=$request->get('name');
+        $doctor->email=$request->get('email');
+        $doctor->save();
+        return redirect('doctor')->with('success', 'Information has been added');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+         $doctor = \App\Doctor::find($id);
+        return view('doctor/edit',compact('doctor','id'));
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $doctor= \App\Doctor::find($id);
+        $doctor->name=$request->get('name');
+        $doctor->email=$request->get('email');
+        $doctor->save();
+        return redirect('doctor');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $doctor = \App\Doctor::find($id);
+        $doctor->delete();
+        return redirect('doctor')->with('success','Information has been  deleted');
+    }
+}
---
+++ database/migrations/2019_01_02_194341_create_appointments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAppointmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_user')->unsigned();
+            $table->foreign('id_user')->references('id')->on('users');
+            $table->integer('id_doctor')->unsigned();
+            $table->foreign('id_doctor')->references('id')->on('doctors');
+            $table->integer('date');
+            $table->integer('time'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appointments');
+    }
+}
